35cbf596a98f4208576a878dd78b63e3dd5dd839
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-02-19 14:19:24 +0700

    Add BCryptPasswordEncoder bean to ku.message.config

--- src/main/java/ku.message/config/SecurityConfig.java
+++ src/main/java/ku.message/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -42,5 +43,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 		web
 				.ignoring()
 				.antMatchers("/h2-console/**");
+	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder(12);
 	}
 }
