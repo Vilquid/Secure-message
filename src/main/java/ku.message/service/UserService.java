59e1612d2e6d081f8dd100aa016872e6020a1d95
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-02-19 14:27:27 +0700

    Update UserService class to use bcrypt hash
    BCryptPasswordEncoder

--- src/main/java/ku.message/service/UserService.java
+++ src/main/java/ku.message/service/UserService.java
@@ -3,6 +3,7 @@ package ku.message.service;
 import ku.message.model.User;
 import ku.message.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +13,7 @@ public class UserService
 	private UserRepository repository;
 
 	@Autowired
-	private HashService hashService;
+	private PasswordEncoder passwordEncoder;
 	public boolean isUsernameAvailable(String username)
 	{
 		return repository.findByUsername(username) == null;
@@ -26,10 +27,8 @@ public class UserService
 		newUser.setLastName(user.getLastName());
 		newUser.setUsername(user.getUsername());
 
-		String salt = hashService.getSalt();
-		String hashedPassword = hashService.getHashedValue(user.getPassword(), salt);
+		String hashedPassword = passwordEncoder.encode(user.getPassword());
 
-		newUser.setSalt(salt);
 		newUser.setPassword(hashedPassword);
 
 		repository.save(newUser);
