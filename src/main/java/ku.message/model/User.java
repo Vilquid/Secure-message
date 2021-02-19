89d5a4a76aadd8aa1322754bcfc303856d655810
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-02-19 13:37:37 +0700

    Add User class in the model package

---
+++ src/main/java/ku.message/model/User.java
@@ -0,0 +1,20 @@
+package ku.message.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String username;
+	private String password;
+	private String salt;
+	private String firstName;
+	private String lastName;
+	// .... generate getter/setter for all attributes
+}
