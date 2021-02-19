5b85aa874b24bcdd147128e9f76ae542037029d0
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-02-19 13:55:20 +0700

    Create UserService class

---
+++ src/main/java/ku.message/service/UserService.java
@@ -0,0 +1,44 @@
+package ku.message.service;
+
+import ku.message.model.User;
+import ku.message.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService
+{
+	@Autowired
+	private UserRepository repository;
+
+	@Autowired
+	private HashService hashService;
+	public boolean isUsernameAvailable(String username)
+	{
+		return repository.findByUsername(username) == null;
+	}
+
+	public int createUser(User user)
+	{
+		User newUser = new User();
+
+		newUser.setFirstName(user.getFirstName());
+		newUser.setLastName(user.getLastName());
+		newUser.setUsername(user.getUsername());
+
+		String salt = hashService.getSalt();
+		String hashedPassword = hashService.getHashedValue(user.getPassword(), salt);
+
+		newUser.setSalt(salt);
+		newUser.setPassword(hashedPassword);
+
+		repository.save(newUser);
+
+		return 1;
+	}
+
+	public User getUser(String username)
+	{
+		return repository.findByUsername(username);
+	}
+}
