25eef127b1c03b2d66b9e5a73212cfd04c1923ec
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-02-19 15:09:16 +0700

    Update home controller
    To check whether a user has logged in
    
    If not, it’ll notify home.html that a user hasn’t logged in and show a login link.
    If so, it’ll send the name of the login user to home.html

--- src/main/java/ku.message/controller/HomeController.java
+++ src/main/java/ku.message/controller/HomeController.java
@@ -1,17 +1,28 @@
 package ku.message.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController
-{
-    @RequestMapping("/home")
-    public String getHomePage(Model model)
-    {
-        // return home.html
-        model.addAttribute("greeting", "Hello");
-        return "home";
-    }
+public class HomeController {
+	@RequestMapping("/home")
+	public String getHomePage(Model model, @AuthenticationPrincipal OAuth2User principal)
+	{
+		if (principal == null)
+		{
+			model.addAttribute("authenticated", false);
+		}
+		else
+		{
+			model.addAttribute("authenticated", true);
+			model.addAttribute("user", principal.getAttribute("name"));
+		}
+
+		model.addAttribute("greeting", "Hello");
+
+		return "home";
+	}
 }
