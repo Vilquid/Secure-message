d7958bef14ac5943fccb1bf41687058384109e20
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-02-19 13:37:14 +0700

    Update SecurityConfig class

--- src/main/java/ku.message/config/SecurityConfig.java
+++ src/main/java/ku.message/config/SecurityConfig.java
@@ -1,39 +1,46 @@
 package ku.message.config;
 
+import ku.message.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+	@Autowired
+	private AuthenticationService authenticationService;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
 		http
 				.authorizeRequests()
-				.antMatchers("/home", "/css/**", "/js/**").permitAll()
+				.antMatchers("/home", "/signup","/css/**", "/js/**").permitAll()
 				.anyRequest().authenticated();
-
 		http.formLogin()
-				.loginPage("/login")
-				.defaultSuccessUrl("/message", true);
+				.defaultSuccessUrl("/message", true)
+				.and().logout();
 	}
 
-//	@Autowired
-//	public void configureGlobal(AuthentificationManagerBuilder auth) throws Exception
-//	{
-//		auth.inMemoryAuthentification()
-//				.withUser("User")
-//				.passwordEncoder(encoder().encode("passw0rd!"))
-//				?roles("USER");
-//	}
-//
-//	@Bean
-//	public PasswordEncoder encoder()
-//	{
-//		return new BCryptPasswordEncoder();
-//	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception
+	{
+		auth.authenticationProvider(this.authenticationService);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception
+	{
+		web
+				.ignoring()
+				.antMatchers("/h2-console/**");
+	}
 }
